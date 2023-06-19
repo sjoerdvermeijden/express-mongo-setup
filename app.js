5b38f90983d81b7402da7d6c4a1fd9736bc0e28e
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2023-06-19 11:58:59 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(8080);

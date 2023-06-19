3fff2320f10e15f4c3e9cf89b21e6686d5a9012b
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2023-06-19 12:50:27 +0200

    Adds a user model to the app

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,14 +1,9 @@
+const User = require("../models/user-model");
+
 exports.getUsers = (req, res) => {
-  res.send([
-    {
-      id: 0,
-      name: "Sjoerd Vermeijden",
-    },
-    {
-      id: 1,
-      name: "Tom Vermeulen",
-    },
-  ]);
+  User.find().then((users) => {
+    res.json(users);
+  });
 };
 
 exports.loginUser = (req, res) => {
